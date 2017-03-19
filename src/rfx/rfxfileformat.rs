c5d65bcf639d1b90a5f75f8434f056397b74a8a2
cyril <cyril@MacBook-2.local>
2017-03-19 12:06:44 +0000

    parser wip

--- src/rfx/rfxfileformat.rs
+++ src/rfx/rfxfileformat.rs
@@ -5,10 +5,17 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Error;
 
-enum RfxParsingState {
+///
+/// RFX file format parser
+///
+///
+
+
+enum RfxParsingContext {
     TopLevel,
     NodeParsed(NodeHandle),
 }
+
 // Stores temporary parsing data
 pub struct RfxFileFormat<'a> {
     reader: BufReader<&'a File>,
@@ -17,7 +24,8 @@ pub struct RfxFileFormat<'a> {
 }
 
 impl<'a> RfxFileFormat<'a> {
-    /// returns a new parser
+
+    /// Returns a new parser data
     pub fn new(file: &File) -> RfxFileFormat {
         RfxFileFormat {
             reader: BufReader::new(file),
@@ -26,37 +34,41 @@ impl<'a> RfxFileFormat<'a> {
         }
     }
 
-    /// Update a project with the content of the file 
+    /// Update a project with the content of the parsed file 
     /// Returns the updated project
     pub fn update(&mut self, mut project: Project) -> Project {
-        let mut context = RfxParsingState::TopLevel;
+        let mut context = RfxParsingContext::TopLevel;
+        // NOTE should we parse tokens instead of lines ?
         while let Ok(bytes) = self.next() {
             if bytes == 0 {
                 return project;
             }
             self.line_bytes = bytes;
+            // let mut lexer = Lexer::new(self.current_line)
+            // for ()
             //
             // Parse depending on the current parser context
             let newcontext = match context {
                 // Expect top level command
-                RfxParsingState::TopLevel => self.parse_top_level(&mut project),
+                RfxParsingContext::TopLevel => self.parse_top_level(&mut project),
                 // a Node was parsed
-                RfxParsingState::NodeParsed(ref node) => {
+                RfxParsingContext::NodeParsed(ref node) => {
                     // Expect parsing parameters
                     let param_parsed = self.parse_node_parameters(&mut project, node);
                     if param_parsed {
-                        RfxParsingState::NodeParsed(node.clone()) // TODO avoid cloning here, it shouldn't be necessary 
+                        RfxParsingContext::NodeParsed(node.clone()) // TODO avoid cloning here, it shouldn't be necessary 
                     } else {
                         self.parse_top_level(&mut project)
                     }
                 }
             };
             context = newcontext;
-        }
+        } // TODO : handle when there was an error while reading the line
         project
     }
 
     /// Next line
+    /// iterator trait ?
     fn next(&mut self) -> Result<usize, Error> {
         self.current_line.clear();
         self.reader.read_line(&mut self.current_line)
@@ -64,29 +76,29 @@ impl<'a> RfxFileFormat<'a> {
 
     /// Parse top level command and setup a context
     /// Returns true if the line has been consumed
-    fn parse_top_level(&self, project: &mut Project) -> RfxParsingState {
+    fn parse_top_level(&self, project: &mut Project) -> RfxParsingContext {
         if self.current_line.starts_with("node") {
-            match self.parse_add_node_cmd(project) {
-                Some(node) => RfxParsingState::NodeParsed(node),
-                None => RfxParsingState::TopLevel, 
+            match self.parse_command_node(project) {
+                Some(node) => RfxParsingContext::NodeParsed(node),
+                None => RfxParsingContext::TopLevel, 
             }
         } else if self.current_line.starts_with("bundle") {
             println!("bundle {}", self.current_line);
             // simple test to alter the project p
             let plugins = Vec::new();
             project.load_plugins(plugins);
-            RfxParsingState::TopLevel
+            RfxParsingContext::TopLevel
         } else if self.current_line.starts_with("#") {
             // Comment next line
-            RfxParsingState::TopLevel
+            RfxParsingContext::TopLevel
         } else if self.current_line.trim().is_empty() {
-            RfxParsingState::TopLevel
+            RfxParsingContext::TopLevel
         } else {
             panic!("unable to parse top level command {}", self.current_line);
         }
     }
 
-    fn parse_add_node_cmd(&self, project: &mut Project) -> Option<NodeHandle> {
+    fn parse_command_node(&self, project: &mut Project) -> Option<NodeHandle> {
         // Read node type
         let plugin_name = unsafe {
             self.current_line.slice_unchecked("node".len() + 1, self.current_line.len() - 1)
