6273006709a170ab068c38c808e041b31f6e2866
cyril <cyril@MacBook-2.local>
2017-03-21 22:53:28 +0000

    improves lexing with new tokens

--- sandbox/lexer/src/main.rs
+++ sandbox/lexer/src/main.rs
@@ -9,15 +9,17 @@ use std::path::PathBuf;
 /// Test parsing in rust
 /// TODO : return error
 ///
-
-
+/// TODO: test "toto".len() is optimized and compiles directly with the len of the string
 #[derive(Debug, PartialEq)]
 pub enum Token {
     OpenBrace,
     CloseBrace,
-    LoadPluginCommand,
-    CreateNodeCommand,
+    PluginCommand,
+    NodeCommand,
     ConnectNodeCommand,
+    PathCommand,
+    ParamCommand,
+    PropertyCommand,
     SemiColon,
     StringLiteral(String),
     FloatNumber(f32),
@@ -27,18 +29,21 @@ pub enum Token {
 
 struct Lexer<'a> {
     input: &'a str,
-    last_pos: usize,
+    begin: usize,
+    end: usize,
 }
 
 // This macro is here because the borrow checked disallow to use a function
 // that does the same. This BC is utterly stupid
+//
+// slice_unchecked could be replaced by input[self.begin..self.end]
 macro_rules! cursor {
     ($var:ident, $ret:expr) => {
         unsafe {
-            if $var.last_pos + 1 > $var.input.len() {
+            if $var.begin + 1 > $var.input.len() {
                 return $ret;
             }
-            $var.input.slice_unchecked($var.last_pos, $var.input.len())
+            $var.input.slice_unchecked($var.begin, $var.input.len())
         }
     }    
 }
@@ -47,21 +52,19 @@ impl<'a> Lexer<'a> {
     pub fn new(input: &'a String) -> Self {
         Lexer {
             input: input,
-            last_pos: 0,
+            begin: 0,
+            end: 0,
         }
     }
 
     fn skip_whitespaces(&mut self) {
         let mut chars = cursor!(self, ()).chars();
         loop {
-            let c = chars.nth(0);
-            match c {
+            match chars.next() {
                 Some(s) => {
                     if s.is_whitespace() {
-                        self.last_pos += 1;
-                        if self.last_pos + 1 > self.input.len() {
-                            break;
-                        }
+                        self.begin += 1;
+                        self.end = self.begin;
                     } else {
                         break;
                     }
@@ -72,69 +75,104 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn next_token(&mut self) -> Token {
-        self.skip_whitespaces();
-        let cursor = cursor!(self, Token::EOF);
-        if cursor.starts_with("loadplugin") {
-            self.last_pos += "loadplugin".len();
-            return Token::LoadPluginCommand;
-        } else if cursor.starts_with("node") {
-            self.last_pos += "node".len();
-            return Token::CreateNodeCommand;
-        } else if cursor.starts_with("\"") {
-            self.last_pos += "\"".len();
-            let mut string_end = self.last_pos;
-            let mut chars = cursor!(self, Token::EOF).chars();
-            loop {
-                match chars.next() {
-                    Some(a) => {
-                        if a == '"' {
+        loop {
+            self.skip_whitespaces(); // could integrate skip_whitespace in the loop
+            let cursor = cursor!(self, Token::EOF);
+            // match a comment
+            if cursor.starts_with("#") {
+                // Eat until end of line comment and continue the main loop
+                let mut chars = cursor.chars();
+                loop {
+                    match chars.next() {
+                        Some('\n') => {
+                            self.end += 1;
                             break;
-                        } else {
-                            string_end += 1;
                         }
+                        Some(c) => self.end += 1,
+                        None => break,  // Error or eof ?
                     }
-                    None => return Token::EOF,
                 }
-            }
-            let captured = unsafe { self.input.slice_unchecked(self.last_pos, string_end) };
-            self.last_pos = string_end + 1;
-            return Token::StringLiteral(captured.to_string());
-
-
-            // TODO : float and int
-
-        } else {
-            // Capture all number characters 
-            let mut chars = cursor.chars();
-            let mut string_end = self.last_pos;
-            loop {
-                match chars.next() {
-                    Some(c) => {
-                        match c {
-                            '0'...'9' | '.' | 'e' | '+' | '-' => {
-                                string_end += 1;
+                self.begin = self.end;
+                continue;
+            } else if cursor.starts_with(";") {
+                self.begin += ";".len();
+                self.end = self.begin;
+                return Token::SemiColon;
+            } else if cursor.starts_with("{") {
+                self.begin += "{".len();
+                self.end = self.begin;
+                return Token::OpenBrace;
+            } else if cursor.starts_with("}") {
+                self.begin += "}".len();
+                self.end = self.begin;
+                return Token::CloseBrace;
+            } else if cursor.starts_with("plugin") {
+                self.begin += "plugin".len();
+                self.end = self.begin;
+                return Token::PluginCommand;
+            } else if cursor.starts_with("path") {
+                self.begin += "path".len();
+                self.end = self.begin;
+                return Token::PathCommand;
+            } else if cursor.starts_with("node") {
+                self.begin += "node".len();
+                self.end = self.begin;
+                return Token::NodeCommand;
+            } else if cursor.starts_with("param") {
+                self.begin += "param".len();
+                self.end = self.begin;
+                return Token::ParamCommand;
+            } else if cursor.starts_with("\"") {
+                self.begin += "\"".len();
+                self.end = self.begin;
+                let mut chars = cursor!(self, Token::EOF).chars();
+                loop {
+                    match chars.next() {
+                        Some(a) => {
+                            if a == '"' {
+                                break;
+                            } else {
+                                self.end += 1;
                             }
-                            _ => break,
                         }
+                        None => return Token::EOF,
                     }
-                    None => break,    
                 }
+                let captured = &self.input[self.begin..self.end];
+                self.end += 1;
+                self.begin = self.end;
+                return Token::StringLiteral(captured.to_string());
+            } else {
+                // Capture all number characters
+                let mut chars = cursor.chars();
+                loop {
+                    match chars.next() {
+                        Some(c) => {
+                            match c {
+                                '0'...'9' | '.' | 'e' | '+' | '-' => {
+                                    self.end += 1;
+                                }
+                                _ => break,
+                            }
+                        }
+                        None => break,    
+                    }
+                }
+                let captured = &self.input[self.begin..self.end];
+                self.begin = self.end;
+                // First try integer
+                let int_parsed = captured.parse::<i32>();
+                if int_parsed.is_ok() {
+                    return Token::IntNumber(int_parsed.unwrap());
+                }
+                // else try float
+                let float_parsed = captured.parse::<f32>();
+                if float_parsed.is_ok() {
+                    return Token::FloatNumber(float_parsed.unwrap());
+                }
+                // TODO : return and error
+                return Token::EOF;
             }
-            //let captured = unsafe { self.input.slice_unchecked(self.last_pos, string_end) };
-            let captured = &self.input[self.last_pos..string_end];
-            self.last_pos = string_end;
-            // First try integer
-            let int_parsed = captured.parse::<i32>();
-            if int_parsed.is_ok() {
-                return Token::IntNumber(int_parsed.unwrap());
-            }
-            // else try float
-            let float_parsed = captured.parse::<f32>();
-            if float_parsed.is_ok() {
-                return Token::FloatNumber(float_parsed.unwrap());
-            }
-            // TODO : return and error
-            return Token::EOF;
         }
     }
 }
@@ -168,32 +206,32 @@ fn test_token_eof() {
 }
 
 #[test]
-fn test_token_loadplugin() {
-    let text1 = "loadplugin".to_string();
+fn test_token_plugin() {
+    let text1 = "plugin".to_string();
     let mut lexer = Lexer::new(&text1);
-    assert!(lexer.next_token() == Token::LoadPluginCommand);
+    assert!(lexer.next_token() == Token::PluginCommand);
 }
 
 #[test]
-fn test_token_loadplugin_with_spaces() {
-    let text1 = "  loadplugin  ".to_string();
+fn test_token_plugin_with_spaces() {
+    let text1 = "  plugin  ".to_string();
     let mut lexer = Lexer::new(&text1);
-    assert!(lexer.next_token() == Token::LoadPluginCommand);
+    assert!(lexer.next_token() == Token::PluginCommand);
 }
 
 #[test]
 fn test_token_createnode() {
     let text1 = "node".to_string();
     let mut lexer = Lexer::new(&text1);
-    assert!(lexer.next_token() == Token::CreateNodeCommand);
+    assert!(lexer.next_token() == Token::NodeCommand);
 }
 
 #[test]
 fn test_two_tokens() {
-    let text1 = "loadplugin node".to_string();
+    let text1 = "plugin node".to_string();
     let mut lexer = Lexer::new(&text1);
-    assert!(lexer.next_token() == Token::LoadPluginCommand);
-    assert!(lexer.next_token() == Token::CreateNodeCommand);
+    assert!(lexer.next_token() == Token::PluginCommand);
+    assert!(lexer.next_token() == Token::NodeCommand);
 }
 
 #[test]
@@ -245,6 +283,7 @@ fn test_int_semicolon_float() {
     let text1 = "202334;982.345".to_string();
     let mut lexer = Lexer::new(&text1);
     assert!(lexer.next_token() == Token::IntNumber(202334));
+    assert!(lexer.next_token() == Token::SemiColon);
     assert!(lexer.next_token() == Token::FloatNumber(982.345));
 }
 
@@ -259,8 +298,67 @@ fn test_file() {
     file.read_to_string(&mut file_content);
     let mut lexer = Lexer::new(&file_content);
 
-    assert!(lexer.next_token() == Token::LoadPluginCommand);
+    assert!(lexer.next_token() == Token::PathCommand);
+    assert!(lexer.next_token() == Token::StringLiteral("".to_string()));
+    assert!(lexer.next_token() == Token::SemiColon);
+    assert!(lexer.next_token() == Token::PluginCommand);
     assert!(lexer.next_token() == Token::StringLiteral("uk.co.thefoundry.BasicGain".to_string()));
-    
+    assert!(lexer.next_token() == Token::SemiColon);
+    assert!(lexer.next_token() == Token::NodeCommand);
+    assert!(lexer.next_token() == Token::StringLiteral("uk.co.thefoundry.BasicGain".to_string()));
+    assert!(lexer.next_token() == Token::OpenBrace);
+    assert!(lexer.next_token() == Token::ParamCommand);
+    assert!(lexer.next_token() == Token::StringLiteral("name".to_string()));
+    assert!(lexer.next_token() == Token::StringLiteral("testnode1".to_string()));
+    assert!(lexer.next_token() == Token::SemiColon);
+    assert!(lexer.next_token() == Token::ParamCommand);
+    assert!(lexer.next_token() == Token::StringLiteral("offset".to_string()));
+    assert!(lexer.next_token() == Token::FloatNumber(0.89));
+    assert!(lexer.next_token() == Token::SemiColon);
+    assert!(lexer.next_token() == Token::ParamCommand);
+    assert!(lexer.next_token() == Token::StringLiteral("color".to_string()));
+    assert!(lexer.next_token() == Token::IntNumber(23));
+    assert!(lexer.next_token() == Token::IntNumber(23));
+    assert!(lexer.next_token() == Token::IntNumber(10));
+
 }
 
+#[test]
+fn test_token_2_lines() {
+    let text1 = "node\nnode\n".to_string();
+    let mut lexer = Lexer::new(&text1);
+    assert!(lexer.next_token() == Token::NodeCommand);
+    assert!(lexer.next_token() == Token::NodeCommand);
+}
+
+#[test]
+fn test_token_2_comments() {
+    let text1 = "# random comment\nnode# and a new comment\nnode".to_string();
+    let mut lexer = Lexer::new(&text1);
+    assert!(lexer.next_token() == Token::NodeCommand);
+    assert!(lexer.next_token() == Token::NodeCommand);
+}
+
+#[test]
+fn test_token_comment() {
+    let text1 = "# random comment\nnode".to_string();
+    let mut lexer = Lexer::new(&text1);
+    assert!(lexer.next_token() == Token::NodeCommand);
+}
+
+#[test]
+fn test_skip_linebreaks () {
+    let text1 = "\n\n\n\n\nnode".to_string();
+    let mut lexer = Lexer::new(&text1);
+    assert!(lexer.next_token() == Token::NodeCommand);
+}
+
+#[test]
+fn test_token_open_close_brace() {
+    let text1 = "node {\nnode}".to_string();
+    let mut lexer = Lexer::new(&text1);
+    assert!(lexer.next_token() == Token::NodeCommand);
+    assert!(lexer.next_token() == Token::OpenBrace);
+    assert!(lexer.next_token() == Token::NodeCommand);
+    assert!(lexer.next_token() == Token::CloseBrace);
+}
